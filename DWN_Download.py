bf19eaae796f8281aa6d23ac0fd00e2b0313cd61
Altynbek Isabekov <aisabekov@ku.edu.tr>
2018-12-08 20:46:29 +0100

    Support for all languages is added.

--- DWN_Download.py
+++ DWN_Download.py
@@ -1,6 +1,11 @@
+# "Deutsch - warum nicht?" course downloader
+# This code downloads all MP3 and PDF files for all explanation languages
+# of the course. Comment or uncomment language to download only a subset.
+# A directory hiearchy is created for every book and chapter.
+# Python 3 should be used.
+
 import urllib
 import xmltodict
-import pprint
 import os
 from bs4 import BeautifulSoup
 
@@ -19,20 +24,41 @@ def utf8_convert(url):
     #print(url_quo, '\n')
     return(url_quo)
 
-dir_parent = 'DWN_Russisch'
-# Parent page with 4 courses (books)
-url_top = 'https://www.dw.com/ru/учить-немецкий/deutsch-warum-nicht/s-2561'
-response = urllib.request.urlopen(utf8_convert(url_top))
-data = response.read()
-text = data.decode('utf-8')
-soup = BeautifulSoup(text, 'html.parser')
-courses_rss = list()
-for tag in soup.find_all('div', {"class": "linkList intern"}):
-    print('Tag:', tag.a['href'])
-    print(tag.h2.contents[0].strip().split('|')[0])
-    courses_rss.append((tag.a['href'], tag.h2.contents[0].strip().split('|')[0]))
+def download_lektion(unterricht, path_sammlung):
+    lektion = unterricht['title']
+    path_name = os.path.join(path_sammlung, lektion)
+    if not os.path.exists(path_name):
+        os.makedirs(path_name)
 
-for book in courses_rss:
+    # Download MP3 file for the Lektion
+    url_mp3 = unterricht['enclosure']['@url']
+    file_name = os.path.basename(url_mp3)
+    print('File: ', file_name, '\n')
+    urllib.request.urlretrieve(url_mp3, os.path.join(path_name, file_name))
+
+    # Lektion
+    url = unterricht['guid']['#text']
+    url_quo = utf8_convert(url)
+
+    response = urllib.request.urlopen(url_quo)
+    data = response.read()
+    text = data.decode('utf-8')
+
+    soup = BeautifulSoup(text, 'html.parser')
+    url_spl = urllib.parse.urlsplit(url)
+    url_spllist = list(url_spl)
+    for tag in soup.find_all('div', {"class": "linkList download"}):
+        print('Tag:', tag.a['href'])
+        url_spllist[2] = tag.a['href']
+        url_spllist[3] = ''
+        url_spllist[4] = ''
+        url_file = urllib.parse.urlunsplit(url_spllist)
+        print('URL: ', url_file)
+        file_name = os.path.join(path_name, os.path.basename(url_file))
+        print('File: ', file_name, '\n')
+        urllib.request.urlretrieve(url_file, file_name)
+
+def download_sammlung(book, dir_parent):
     url_buch = book[0]
     buch = book[1]
 
@@ -45,34 +71,54 @@ for book in courses_rss:
     print("Book: ", doc['rss']['channel']['item'][0]['title'])
 
     for unterricht in doc['rss']['channel']['item']:
-        lektion = unterricht['title']
-        if not os.path.exists(os.path.join(dir_parent, buch, lektion)):
-            os.makedirs(os.path.join(dir_parent, buch, lektion))
+        download_lektion(unterricht, os.path.join(dir_parent, buch))
 
-        # Download MP3 file for the Lektion
-        url_mp3 = unterricht['enclosure']['@url']
-        file_name = os.path.basename(url_mp3)
-        print('File: ', file_name, '\n')
-        urllib.request.urlretrieve(url_mp3, os.path.join(dir_parent, buch, lektion, file_name))
+languages = [
+#             ('DWN_Deutsch',             'https://www.dw.com/de/deutsch-lernen/deutsch-warum-nicht/s-2163'),
+             ('DWN_Englisch',            'https://www.dw.com/en/learn-german/deutsch-warum-nicht/s-2548'),
+#             ('DWN_Franzoesisch',        'https://www.dw.com/fr/apprendre-lallemand/deutsch-warum-nicht/s-2618'),
+#             ('DWN_Spanisch',            'https://www.dw.com/es/aprender-alemán/deutsch-warum-nicht/s-4642'),
+#             ('DWN_Rumänisch',           'https://www.dw.com/ro/învaţă-germană/deutsch-warum-nicht/s-2703'),
+#             ('DWN_Portugiesisch',       'https://www.dw.com/pt-br/aprender-alemão/deutsch-warum-nicht/s-2595'),
+             ('DWN_Russisch',            'https://www.dw.com/ru/учить-немецкий/deutsch-warum-nicht/s-2561'),
+#             ('DWN_Ukrainisch',          'https://www.dw.com/uk/вивчати-німецьку/deutsch-warum-nicht/s-9958'),
+#             ('DWN_Polnisch',            'https://www.dw.com/pl/nauka-niemieckiego/deutsch-warum-nicht/s-2740'),
+#             ('DWN_Serbisch',            'https://www.dw.com/sr/učite-nemački/deutsch-warum-nicht/s-3787'),
+#             ('DWN_Bosnisch',            'https://www.dw.com/bs/učite-njemački/deutsch-warum-nicht/s-2640'),
+#             ('DWN_Kroatisch',           'https://www.dw.com/hr/učenje-njemačkog/deutsch-warum-nicht/s-3814'),
+#             ('DWN_Bulgarisch',          'https://www.dw.com/bg/да-учим-немски/deutsch-warum-nicht/s-2653'),
+#             ('DWN_Makedonisch',         'https://www.dw.com/mk/учење-германски/deutsch-warum-nicht/s-10224'),
+#             ('DWN_Griechisch',          'https://www.dw.com/el/μαθαινω-γερμανικα/deutsch-warum-nicht/s-2726'),
+#             ('DWN_Albanisch',           'https://www.dw.com/sq/mësoni-gjermanisht/deutsch-warum-nicht/s-3670'),
+#             ('DWN_Persisch',            'https://www.dw.com/fa-ir/آموزش-آلمانی/deutsch-warum-nicht/s-2770'),
+#             ('DWN_Dari',                'https://www.dw.com/fa-af/آموزش-زبان-آلمانی/deutsch-warum-nicht/s-11360'),
+#             ('DWN_Paschtu',             'https://www.dw.com/ps/د-الما-ني-ژ-بی-زده-کړه/deutsch-warum-nicht/s-10874'),
+#             ('DWN_Urdu',                'https://www.dw.com/ur/جرمن-سیکھئے/deutsch-warum-nicht/s-10928'),
+#             ('DWN_Hindi',               'https://www.dw.com/hi/जर्मन-सीखिये/deutsch-warum-nicht/s-11085'),
+#             ('DWN_Bengali',             'https://www.dw.com/bn/জার্মান-ভাষা-শিখুন/deutsch-warum-nicht/s-11014'),
+#             ('DWN_Türkisch',            'https://www.dw.com/tr/almanca-öğrenin/deutsch-warum-nicht/s-2609'),
+#             ('DWN_Arabisch',            'https://www.dw.com/ar/تعلُّم-الألمانية/deutsch-warum-nicht/s-8613'),
+#             ('DWN_Amharisch',           'https://www.dw.com/am/ጀርመንኛ-መማር/deutsch-warum-nicht/s-2776'),
+#             ('DWN_Swahili',             'https://www.dw.com/sw/kujifunza-kijerumani/deutsch-warum-nicht/s-2683'),
+#             ('DWN_Hausa',               'https://www.dw.com/ha/koyon-jamusanci/deutsch-warum-nicht/s-3845'),
+#             ('DWN_Chinesisch',          'https://www.dw.com/zh/德语天地/deutsch-warum-nicht/s-2584'),
+#             ('DWN_Indonesisch',         'https://www.dw.com/id/bahasa-jerman/deutsch-warum-nicht/s-2717')
+]
 
-        # Lektion
-        url = unterricht['guid']['#text']
-        url_quo = utf8_convert(url)
+for sprache in languages:
+    # Directory to download to
+    dir_parent = sprache[0]
+    # Parent page with 4 courses (books)
+    url_top = sprache[1]
+    response = urllib.request.urlopen(utf8_convert(url_top))
+    data = response.read()
+    text = data.decode('utf-8')
+    soup = BeautifulSoup(text, 'html.parser')
+    courses_rss = list()
+    for tag in soup.find_all('div', {"class": "linkList intern"}):
+        print('Tag:', tag.a['href'])
+        print(tag.h2.contents[0].strip().split('|')[0])
+        courses_rss.append((tag.a['href'], tag.h2.contents[0].strip().split('|')[0]))
 
-        response = urllib.request.urlopen(url_quo)
-        data = response.read()
-        text = data.decode('utf-8')
-
-        soup = BeautifulSoup(text, 'html.parser')
-        url_spl = urllib.parse.urlsplit(url)
-        url_spllist = list(url_spl)
-        for tag in soup.find_all('div', {"class": "linkList download"}):
-            print('Tag:', tag.a['href'])
-            url_spllist[2] = tag.a['href']
-            url_spllist[3] = ''
-            url_spllist[4] = ''
-            url_file = urllib.parse.urlunsplit(url_spllist)
-            print('URL: ', url_file)
-            file_name = os.path.join(dir_parent, buch, lektion, os.path.basename(url_file))
-            print('File: ', file_name, '\n')
-            urllib.request.urlretrieve(url_file, file_name)
+    for book in courses_rss:
+        download_sammlung(book, dir_parent)
---
+++ README.md
@@ -0,0 +1,52 @@
+# "Deutsch - warum nicht?" course downloader  
+This code downloads all MP3 and PDF files for all narration languages of the course.  
+A directory hiearchy is created for every book and all chapters.  
+Python 3 should be used.  
+  
+  
+Example for Russian narration:  
+  
+DWN_Russisch  
+├── Deutsch - warum nicht? Часть 1  
+│   ├── Урок 01 - Это песня  
+│   │   ├── dwn-1-russisch-lektion-01.pdf  
+│   │   ├── DWN_Russisch_Serie1_Lektion01-podcast-2885-269026.mp3  
+│   │   └── s1l1.pdf  
+│   :   :  
+│   └── Урок 26 - Я Вас приглашаю  
+│       ├── dwn-1-russisch-lektion-26.pdf  
+│       ├── DWN_Russisch_Serie1_Lektion26-podcast-2885-269669.mp3  
+│       └── s1l26.pdf  
+│  
+├── Deutsch - warum nicht? Часть 2  
+│   ├── Урок 01 - Кто ты?  
+│   │   ├── dwn-2-russisch-lektion-01.pdf  
+│   │   ├── DWN_Russisch_Serie2_Lektion01-podcast-2886-269671.mp3  
+│   │   └── s2l1.pdf  
+│   :   :  
+│   └── Урок 26 - Плавание к утёсу Лорелеи – это замечательно  
+│       ├── dwn-2-russisch-lektion-26.pdf  
+│       ├── DWN_Russisch_Serie2_Lektion26-podcast-2886-272421.mp3  
+│       └── s2l26.pdf  
+│  
+├── Deutsch - warum nicht? Часть 3  
+│   ├── Урок 01 - Музыка – супер!  
+│   │   ├── dwn-3-russisch-lektion-01.pdf  
+│   │   ├── DWN_Russisch_Serie3_Lektion01-podcast-2887-272872.mp3  
+│   │   └── s3l1.pdf  
+│   :   :  
+│   └── Урок 26 - Мы просто хотим быть вместе  
+│       ├── dwn-3-russisch-lektion-26.pdf  
+│       ├── DWN_Russisch_Serie3_Lektion26-podcast-2887-273499.mp3  
+│       └── s3l26.pdf  
+│  
+├── Deutsch - warum nicht? Часть 4  
+│   ├── Урок 01 - Это блестящая идея  
+│   │   ├── dwn-4-russisch-lektion-01.pdf  
+│   │   ├── DWN_Russisch_Serie4_Lektion01-podcast-2888-273578.mp3  
+│   │   └── s4l1.pdf  
+│   :   :  
+│   └── Урок 26 - Мы просто хотим быть вместе  
+│       ├── dwn-4-russisch-lektion-26.pdf  
+│       ├── DWN_Russisch_Serie4_Lektion26-podcast-2888-273672.mp3  
+└       └── s4l26.pdf  
